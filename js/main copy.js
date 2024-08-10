9b2e84753e511c90cac5af1fb4ad20a1ab1db51a
Mohamed Hossam <mohamed.webdev2@gmail.com>
2024-08-10 20:41:51 +0300

    Add files via upload

---
+++ js/main copy.js
@@ -0,0 +1,30 @@
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".loading").style.visibility = "visible";
+    } else {
+        $(".loading").fadeOut();
+        document.querySelector("body").style.overflow = "visible";
+    }
+};
+$(document).ready(function () {
+    for (let i = 2; i <= 300; i++) {
+        $(".avatar .child").append("<img src='img/avatar/mohamedHossam (" + i + ").webp' >");
+    }
+
+    let scrollFollow = 0;
+    let fullScroll = $("html").height() - $(window).height();
+
+    function updateActiveSlide(scrollNow) {
+        let slide = Math.round(scrollNow / fullScroll * 299) + 1;
+        $(".avatar img").removeClass("active");
+        $(".avatar .child img:nth-child(" + slide + ")").addClass("active");
+    }
+
+    $(window).on("scroll", function () {
+        let scrollNow = $(this).scrollTop();
+        scrollFollow = scrollNow;
+
+        updateActiveSlide(scrollFollow);
+    });
+});
