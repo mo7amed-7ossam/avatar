6df2ec6c640021a2f5e38e3013ff8e532e735c68
Mohamed Hossam <mohamed.webdev2@gmail.com>
2024-08-10 20:34:10 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.overflow = "hidden";
+        document.querySelector(".loading").style.visibility = "visible";
+    } else {
+        $(".loading").fadeOut();
+        document.querySelector("body").style.overflow = "visible";
+    }
+};
+
 for (i = 2; i <= 300; i++) {
     $(".avatar .child").append("<img src='img/avatar/mohamedHossam (" + i + ").webp' >")
 }
@@ -19,11 +29,10 @@ $(window).scroll(function () {
             }
         }, 50)
 
-    } else if ((scrollNow < scrollFollow )) {
+    } else if ((scrollNow < scrollFollow)) {
         go_up = setInterval(function () {
             if (scrollFollow > scrollNow) {
                 scrollFollow--;
-                console.log(scrollFollow)
                 let slide = Math.round(scrollFollow / fullScroll * 300) + 1;
                 $(".avatar img").removeClass("active")
                 $(".avatar .child img:nth-child(" + slide + ")").addClass("active")
@@ -34,13 +43,4 @@ $(window).scroll(function () {
     }
 
 })
-
-
-
-// $(window).scroll(function () {
-    
-// })
-
-
-
 
