2f202bdd448b5c3c1f69db6cae07e83ab00ce641
Mohamed Hossam <mohamed.webdev2@gmail.com>
2024-08-10 15:45:08 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ for (i = 2; i <= 300; i++) {
 $(window).scroll(function () {
     let fullScroll = $("html").height() - $(window).height();
     let top = Math.round($(this).scrollTop() / fullScroll * 300);
-    console.log(top)
     if (top > 0) {
         $(".avatar img").removeClass("active")
         $(".avatar img:nth-child(" + top + ")").addClass("active")
